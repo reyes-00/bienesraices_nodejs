c35f670d811ec426163493a40c9611cf2a88808b
reyes-00 <boower-00@hotmail.com>
2023-09-20 16:55:43 -0600

    actualizacion y eliminacion

--- controllers/propiedadesController.js
+++ controllers/propiedadesController.js
@@ -2,35 +2,37 @@ import {Propiedad, Precio, Categoria, Usuario} from '../models/index.js'
 import subidaExcel from '../helpers/Excel.js';
 import Excel from "../models/Excel.js";
 import {validationResult} from 'express-validator'
-import { constants } from 'fs';
+import { unlink } from 'node:fs/promises';
 
 const propiedadesIndex = async(req,res) =>{
-  console.log(req.usuario);
+  const id = req.usuario.id;
+
+  
   let consultadDatos = [];
   consultadDatos = await Excel.findAll()
-
+  
   //console.log(consultadDatos);
   try{
+    const PropiedadesUsuarios = await Propiedad.findAll({
+      where : {usuarioId : id}, 
+      include:[
+        {model : Categoria, as: "categoria"},
+        {model: Precio, as: "precio"}
+      ]
+    })
     consultadDatos = await Excel.findAll();
 
       res.render('propiedades/index',{
         pagina : 'Tus Proiedades',
-        consultadDatos
-        
+        csrfToken : req.csrfToken(),
+        consultadDatos,
+        PropiedadesUsuarios
       })
     
   }catch(err){
     console.log(err);
   }
   
-
- 
-
-/*  res.render('propiedades/index',{
-    pagina : 'Tus Proiedades',
-    datosExcel
-    
-  })*/
 }
 
 const crear = async(req, res) => {
@@ -161,8 +163,126 @@ const SubirPropiedad = async(req, res, next) =>{
     }
 }
 
+const editarPropiedad = async(req, res) => {
+
+  //validar que exita la propiedad
+  const {id} = req.params
+
+  const propiedad = await Propiedad.findByPk(id)
+  if(!propiedad){
+    return res.redirect("/mis-propiedades")
+  }
+  
+  //Validar que sea del usuario que la creo
+  
+  const UsuarioActual = req.usuario.id
+  
+  if( UsuarioActual.toString() !== propiedad.usuarioId.toString()){
+    return res.redirect("/mis-propiedades")
+
+  }
+
+  const [categorias, precios] = await Promise.all([
+    Categoria.findAll(),
+    Precio.findAll()
+  ])
+  res.render('propiedades/editar',{
+    pagina: `Editar propiedad: ${propiedad.titulo} `,
+    categorias,
+    precios,
+    csrfToken: req.csrfToken(),
+    
+    datos:propiedad
+  })
+}
+
+const updatePropiedad = async (req, res) =>{
+  
+  //Validamos los campos del formulario
+  
+  const result = validationResult(req)
+  if(!result.isEmpty()) {
+    const [categorias, precios] = await Promise.all([
+      Categoria.findAll(),
+      Precio.findAll()
+    ])
+    res.render('propiedades/editar',{
+      pagina: "Ediatr Propiedad",
+      categorias,
+      csrfToken : req.csrfToken(),
+      precios,
+      errores: result.errors,
+      datos: req.body
+    })
+  }
+
+   //validar que exita la propiedad
+   const {id} = req.params
+
+   const propiedad = await Propiedad.findByPk(id)
+   if(!propiedad){
+     return res.redirect("/mis-propiedades")
+   }
+   
+   //Validar que sea del usuario que la creo
+   
+   const UsuarioActual = req.usuario.id
+   
+   if( UsuarioActual.toString() !== propiedad.usuarioId.toString()){
+     return res.redirect("/mis-propiedades")
+   }
+
+   //actualizar en la base
+   const{titulo, descripcion,categoria: categoriaId,precio: precioId,estacionamiento,wc,habitaciones,calle,lat,lng}=req.body
+
+   try{
+    propiedad.set({
+      titulo,
+      descripcion,
+      categoriaId,
+      precioId,
+      estacionamiento,
+      wc,
+      habitaciones,
+      calle,
+      lat,
+      lng
+    })
+
+    await propiedad.save()
+
+    return res.redirect("/mis-propiedades")
+
+   }catch(err){
+    console.log(err);
+   }
+}
+
+const eliminar = async (req, res) => {
+   //validar que exita la propiedad
+   const {id} = req.params
+
+   const propiedad = await Propiedad.findByPk(id)
+   if(!propiedad){
+     return res.redirect("/mis-propiedades")
+   }
+   
+   //Validar que sea del usuario que la creo
+   
+   const UsuarioActual = req.usuario.id
+   
+   if( UsuarioActual.toString() !== propiedad.usuarioId.toString()){
+     return res.redirect("/mis-propiedades")
+   }
+    if(propiedad.imagen){
+      await unlink (`public/uploads/${propiedad.imagen}`)
+    }
+
+   await propiedad.destroy();
+   return res.redirect("/mis-propiedades")
 
 
+}
 
 export {
   propiedadesIndex,
@@ -170,5 +290,8 @@ export {
   guardar,
   agregarImagen,
   SubirPropiedad,
+  editarPropiedad,
+  updatePropiedad,
+  eliminar
  
 }
--- routes/PropiedadesRoutes.js
+++ routes/PropiedadesRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import {body} from 'express-validator'
 import ProtegerRuta from '../middleware/ProtegerRuta.js';
-import {propiedadesIndex, crear, guardar, agregarImagen, SubirPropiedad} from '../controllers/propiedadesController.js'
+import {propiedadesIndex, crear, guardar, agregarImagen, SubirPropiedad,editarPropiedad, updatePropiedad, eliminar} from '../controllers/propiedadesController.js'
 import upload from '../middleware/subirImagen.js'
 
 const router = express.Router();
@@ -23,8 +23,21 @@ router.post('/propiedades/crear', ProtegerRuta,
             );
 
 router.get("/propiedades/agregar-imagen/:id",ProtegerRuta, agregarImagen)
-
+            
 router.post("/propiedades/agregar-imagen/:id",ProtegerRuta, upload.single("imagen"), SubirPropiedad)
 
+router.get("/propiedades/editar/:id",ProtegerRuta, editarPropiedad)
+router.post("/propiedades/editar/:id",ProtegerRuta,
+  body("titulo").notEmpty().withMessage('El titulo es requerido'),
+  body("descripcion").notEmpty().withMessage('La descripcion es requerida').isLength({min: 20}).withMessage('La descripcion es muy corta'),
+  body('categoria').isNumeric().withMessage('Selecciona una categoria'),
+  body('precio').isNumeric().withMessage('Selecciona una precio'),
+  body('estacionamiento').isNumeric().withMessage('Selecciona una estacionamiento'),
+  body('wc').isNumeric().withMessage('Selecciona un numero de wc'),
+  body('habitaciones').isNumeric().withMessage('Selecciona una habitacion'),
+  body('lat').notEmpty().withMessage('Selecciona una calle en el Mapa'),
+  updatePropiedad)
+
+router.post("/propiedades/eliminar/:id",ProtegerRuta, eliminar)
 
 export default router;
